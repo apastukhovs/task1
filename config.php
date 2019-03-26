1eee0b868dbf491be75ccd11c00fbd27b265ebd4
apastukhovs <a.pastukhov.s@gmail.com>
2019-03-26 14:19:43 +0200

    Update config.php
    added "constant"

--- config.php
+++ config.php
@@ -1,3 +1,9 @@
 <?
 define('DirPath',__DIR__ . '/files/');
+define("ERR_INI_SIZE", "File exceeded upload_max_filesize.");
+define("ERR_FORM_SIZE", "File exceeded max_file_size.");
+define("ERR_PARTIAL", "File only partially uploaded.");
+define("ERR_NO_FILE", "No file uploaded.");
+define("ERR_NO_TMP_DIR", "Missing temporary folder.");
+define('TITLE', 'Upload files');
 ?>
