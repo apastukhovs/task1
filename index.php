a65d24c69d197afa001f094182fa74f420195ef6
apastukhovs <a.pastukhov.s@gmail.com>
2019-03-26 09:40:13 +0200

    Update index.php
    added errors check

--- index.php
+++ index.php
@@ -1,10 +1,52 @@
-<?
-include './function.php';
-include './config.php';
+<?php
 
-uploadFile();
-getListOfFile($dir);
-getFileSize($dir);
-include './templates/index.php';
+require 'config.php';
+require 'function.php';
+
+if(isset($_FILES['file'])){
+    $errors = array();
+	$data = '';
+    $file_name = $_FILES['file']['name'];
+
+	if($_FILES['file']['error'] > 0){
+	    switch ($_FILES['userfile']['error'])
+		{
+      		case 1: $errors[] = ERR_INI_SIZE;  break;
+      		case 2: $errors[] = ERR_FORM_SIZE;  break;
+      		case 3: $errors[] = ERR_PARTIAL;  break;
+      		case 4: $errors[] = ERR_NO_FILE;  break;
+      		case 6: $errors[] = ERR_NO_TMP_DIR;  break;
+			default:
+            	$errors[] = 'Unknown errors.';
+    	}
+
+	}
+
+	if(file_exists(DirPath . $file_name)){
+    	$errors[] = 'File already exists.';
+	}
+
+    if(empty($errors)){
+    	$uploadfile = uploadFile(DirPath);
+		if($uploadfile)
+			$data = "The file " . basename($uploadfile) . " has been uploaded";
+		} else {
+			$errors[] = "Permission denied";
+		}
+    }
+}
+
+if(isset($_POST["fname"])){
+	$fname = $_POST["fname"];
+	$dir = DirPath;
+	if(removeFile($dir, $fname)){
+		$data = "File deleted";
+	} else {
+		$errors[] = "You don't have permission to delete this file";
+	}
+}
+
+
+include 'tmpl/template.php';
 
 ?>
